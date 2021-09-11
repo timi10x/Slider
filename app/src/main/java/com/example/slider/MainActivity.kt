c3652acde4a5c82c5c3785f00563602cfd88ca1f
timmyCoder <olatoyedan@gmail.com>
2021-09-11 20:20:47 +0100

    added demo video

--- app/src/main/java/com/example/slider/MainActivity.kt
+++ app/src/main/java/com/example/slider/MainActivity.kt
@@ -28,34 +28,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-
         val seekBar = findViewById<CustomSeekBar>(R.id.seekBar)
 
-        seekBar.max = TIME_MILLS
         seekBar?.addOnPreviewTextChangeListener(object : CustomSeekBar.OnPreviewTextChangeListener {
             override fun onPreviewTextChanged(seekBar: CustomSeekBar, progress: Int): String {
                 val rn: Int = Random.nextInt(900000) + 100000
-                val duration = seekBar.progress.toLong()
-
-                val hours = java.util.concurrent.TimeUnit.MILLISECONDS.toHours(duration)
-                val minutes =
-                    java.util.concurrent.TimeUnit.MILLISECONDS.toMinutes(duration) - java.util.concurrent.TimeUnit.HOURS.toMinutes(
-                        java.util.concurrent.TimeUnit.MILLISECONDS.toHours(duration)
-                    )
-                val seconds =
-                    java.util.concurrent.TimeUnit.MILLISECONDS.toSeconds(duration) - java.util.concurrent.TimeUnit.MINUTES.toSeconds(
-                        java.util.concurrent.TimeUnit.MILLISECONDS.toMinutes(duration)
-                    )
-
                 return String.format(rn.toString())
             }
         })
-    }
-
-    companion object {
-        private const val TIME_MILLS = 5025000
     }
 }
 
